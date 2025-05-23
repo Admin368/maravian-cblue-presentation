c0bb42e4c611957f69e486fea291b629af93a570
paulk_naanac <58757588+Admin368@users.noreply.github.com>
2025-05-23 08:31:37 +0800

    server => watch: false,

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "maravian-presentation",
       script: "./server.js",
-      watch: true,
+      watch: false,
       instances: 1,
       autorestart: true,
       max_restarts: 10,
